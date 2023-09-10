06ee417974413e97deff722f661a3bd899c36776
nussnk <nuss@yandex.ru>
2023-09-10 13:54:54 +0300

    typos fixed

--- prov-log.sh
+++ prov-log.sh
@@ -2,11 +2,13 @@
 
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 
-systemctl restart chronyd
+systemctl restart chronyd.service
 
 yum install epel-release -y
 yum install vim -y
 
+cp /vagrant/log/rsyslog.conf /etc/
 cp /vagrant/log/auditd.conf /etc/audit/
+systemctl restart rsyslog.service
 service auditd restart
 
